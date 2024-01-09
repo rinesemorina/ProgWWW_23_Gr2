580c4188e9cd6d7cd88c57ce258debe1cc7f1ccc
ronalatifaj <rona.latifaj@student.uni-pr.edu>
2024-01-10 00:06:54 +0100

    login integrated into page flow

--- tabs/login-page/login-page.js
+++ tabs/login-page/login-page.js
@@ -1,19 +1,20 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-const signinBtn = document.getElementById('sign-in-button');
-const signupBtn = document.getElementById('sign-up-button');
+const container = document.getElementById('login-container');
+const registerBtn = document.getElementById('login-register');
+const loginBtn = document.getElementById('login-login');
+const signinBtn = document.getElementById('login-sign-in-button');
+const signupBtn = document.getElementById('login-sign-up-button');
 
 registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+    container.classList.add("login-active");
 });
 
 loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    container.classList.remove("login-active");
 });
 
 signupBtn.onclick = () => {
     window.location.replace("http://127.0.0.1:5500/index.html");
+    console.log('signin');
 };
 signinBtn.onclick = () => {
     location.href = "http://127.0.0.1:5500/index.html";
